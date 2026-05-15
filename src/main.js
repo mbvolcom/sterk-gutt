ff304a050e755af42f9ed41e542286020b0cbb2a
mbvolcom <mariusbjornson@gmail.com>
2026-05-15 12:30:02 +0200

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,6 @@
+// ══ ENTRY POINT ══════════════════════════════════════════════════════════════
+import './styles/base.css';
+import './styles/layout.css';
+import './styles/components.css';
+import './styles/workout.css';
+import './app.js';
